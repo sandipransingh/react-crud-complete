6c6bcbe1ea48a3a44287edec139be6e2c7c7ec4c
sandipransingh <sandipk.ransingh44@gmail.com>
2023-10-03 19:06:59 +0530

    crud files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Component/Header';
+import Home from './Component/default/Home';
+import Login from './Component/Auth/Login';
+import Register from './Component/Auth/Register';
+import Pnf from './Component/default/Pnf';
+import './App.css'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Header/>
+
+        <Routes>
+          <Route path={`/`} element={<Home/>} />
+          <Route path={`/login`} element={<Login/>} />
+          <Route path={`/register`} element={<Register/>} />
+          <Route path={`/*`} element={<Pnf/>} />
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
